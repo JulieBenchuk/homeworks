c8d7ee0c4441fcbe6bc96a78692b6993ccdfbde2
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-02 19:20:20 +0300

    added HW1

---
+++ src/p2-homeworks/h1/HW1.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Message, {MessagePropsType} from "./Message"
+
+
+function HW1() {
+    const messageData = {
+        avatar: "https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg",
+        name: "Artem",
+        message: "npm start нажимал?",
+        time: "22:00",
+    };
+    return (
+        <div>
+            <hr/>
+            homeworks 1
+            {/*should work (должно работать)*/}
+
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <hr/>
+            {/*для личного творчества, могу проверить*/}
+            {/*<AlternativeMessage/>*/}
+            <hr/>
+        </div>
+    )
+}
+
+export default HW1
---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import classes from "./Message.module.css"
+
+
+export type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
+    return (
+        <div className={classes.message}>
+            <span className={classes.avatar}>
+                <img src={props.avatar}/>
+            </span>
+            <div className={classes.triangle}></div>
+            <span>
+                <div className={classes.messageInner}>
+                  <div className={classes.name}>{props.name}</div>
+                  <div className={classes.current_message}>{props.message}</div>
+                    <div className={classes.time}>{props.time}</div>
+                </div>
+            </span>
+        </div>
+    )
+}
+
+export default Message
