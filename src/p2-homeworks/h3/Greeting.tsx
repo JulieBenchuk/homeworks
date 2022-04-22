58d7fbd3c7d8c54580243dbf20cb9b0aad614cfd
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-22 12:46:29 +0300

    HW-3-4 add some CSS

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,7 +1,7 @@
 import React, {ChangeEvent, KeyboardEvent, ChangeEventHandler} from 'react'
 import SuperInputText from '../h4/common/c1-SuperInputText/SuperInputText';
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
-import s from './Greeting.module.css'
+import s from './Greeting.module.css';
 
 export type GreetingPropsType = {
     name: string // need to fix any
@@ -15,21 +15,23 @@ export type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
 ) => {
-    const inputClass = name ? s.someClass : s.error; // need to fix with (?:)
+    /*const inputClass = name ? s.someClass : s.error; // need to fix with (?:)*/
     const onKeyPresHandler = (e: KeyboardEvent<HTMLInputElement>) => {
         e.key === "Enter" && addUser();
     }
 
     return (
         <div className={s.HW3}>
-            <SuperInputText placeholder={"Enter your name..."} value={name} onChange={setNameCallback}
-                   onKeyPress={onKeyPresHandler}/>
+            <div className={s.inputButton}>
+                <SuperInputText placeholder={"Enter your name..."} value={name} onChange={setNameCallback}
+                                onKeyPress={onKeyPresHandler} error={error}/>
             <SuperButton onClick={addUser}>ADD</SuperButton>
-            <div className={s.totalAll}>
-                <span className={s.total}>TOTAL</span>
-                <span className={s.totalUsers}>{totalUsers}</span>
             </div>
-            <div className={s.errorMessage}>{error}</div>
+            <div className={s.totalAll}>
+                <span className={s.totalUsers}>{totalUsers}</span>
+                <span className={s.total}>TOTAL</span>
+            </div>
+           {/* <div className={s.errorMessage}>{error}</div>*/}
         </div>
     )
 }
--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -6,7 +6,7 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
-type SuperInputTextPropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
+export type SuperInputTextPropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
     onChangeText?: (value: string) => void
     onEnter?: () => void
     error?: string
@@ -51,7 +51,7 @@ const SuperInputText: React.FC<SuperInputTextPropsType> = (
 
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
             />
-            {error && <span className={finalSpanClassName}>{error}</span>}
+            {error && <div className={finalSpanClassName}>{error}</div>}
         </>
     )
 }
