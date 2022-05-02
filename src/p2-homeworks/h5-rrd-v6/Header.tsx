af96b4f8f23783c0270a323c2d8df139d61e33c7
JulieBenchuk <juliebenchuk@gmail.com>
2022-05-03 00:37:17 +0300

    some CSS

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import s from "./Header.module.css"
 
 function Header() {
     return (
-        <div>
+        <div className={s.block_header}>
             <nav>
                 <div>
-                    <NavLink to='/pre-junior'>
+                    <NavLink to='/pre-junior' className={s.navlink}>
                         Pre-Junior
                     </NavLink>
                 </div>
                 <div>
-                    <NavLink to='/junior'>
+                    <NavLink to='/junior' className={s.navlink}>
                         Junior
                     </NavLink>
                 </div>
                 <div>
-                    <NavLink to='/junior-plus'>
+                    <NavLink to='/junior-plus' className={s.navlink}>
                         Junior-Plus
                     </NavLink>
                 </div>
--- src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
+++ src/p2-homeworks/h5-rrd-v6/pages/Error404.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import s from './Error404.module.css'
 
 function Error404() {
     return (
-        <div>
+        <div className={s.error404}>
             <div>404</div>
-            <div>Page not found!</div>
+            <div>Page not found :(</div>
             <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
         </div>
     )
