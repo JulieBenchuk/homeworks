0213abf2c81efda815876da9338bc2b6798f8d9f
JulieBenchuk <juliebenchuk@gmail.com>
2022-05-03 16:10:02 +0300

    add pop-up menu

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -4,24 +4,29 @@ import s from "./Header.module.css"
 
 function Header() {
     return (
-        <div className={s.block_header}>
-            <nav>
-                <div>
-                    <NavLink to='/pre-junior' className={s.navlink}>
-                        Pre-Junior
-                    </NavLink>
-                </div>
-                <div>
-                    <NavLink to='/junior' className={s.navlink}>
-                        Junior
-                    </NavLink>
-                </div>
-                <div>
-                    <NavLink to='/junior-plus' className={s.navlink}>
-                        Junior-Plus
-                    </NavLink>
-                </div>
-            </nav>
+        <div className={s.header}>
+            <ul className={s.menu}>
+                <li>
+                    <span className={s.level}>LEVEL</span>
+                    <ul>
+                        <li>
+                            <NavLink to='/pre-junior'>
+                                Pre-Junior
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/junior'>
+                                Junior
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to='/junior-plus'>
+                                Junior-Plus
+                            </NavLink>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     )
 }
